8c8725583222c2ff4076e97301bf9b02c17e185a
ArtackSM13 <ArtackSM13@yandex.ua>
2020-07-31 21:11:15 +0300

    tesla verstaka marafon

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(function(){
+	$('.slider').slick({
+	  arrows: false,
+    fade: true,
+    autoplay: 3000,
+    dots: true
+     
+	});
+
+
+	$('.header__menu').on('click',function(){
+
+		$('.menu').addClass('active');
+
+	});
+  $('.close-btn').on('click',function(){
+
+    $('.menu').removeClass('active');
+
+  });
+
+
+});
